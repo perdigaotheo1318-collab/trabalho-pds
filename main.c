671276bdade9c5e50892e3298ad6c01a50e0e0a2
perdigaotheo1318-collab <perdigaotheo1318@gmail.com>
2025-12-03 21:28:31 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 #include <allegro5/allegro_primitives.h>
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_ttf.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #include "constants.h"
 #include "renderer.h"
@@ -13,13 +16,13 @@
 #include "teclado.h"
 
 int main() {
-    srand(time(NULL));
+    srand((unsigned)time(NULL));
 
     must_init(al_init(), "allegro");
-    must_init(al_init_image_addon(), "image");
-    must_init(al_init_primitives_addon(), "primitives");
-    must_init(al_init_font_addon(), "font");
-    must_init(al_init_ttf_addon(), "ttf");
+    must_init(al_init_image_addon(), "allegro_image");
+    must_init(al_init_primitives_addon(), "allegro_primitives");
+    must_init(al_init_font_addon(), "allegro_font");
+    must_init(al_init_ttf_addon(), "allegro_ttf");
     must_init(al_install_keyboard(), "keyboard");
 
     ALLEGRO_TIMER* timer = al_create_timer(1.0 / 60.0);
@@ -29,41 +32,81 @@ int main() {
     must_init(queue, "queue");
 
     ALLEGRO_EVENT event;
-    unsigned char keys[ALLEGRO_KEY_MAX];
-    ClearKeyboardKeys(keys);
+    unsigned char keyboard_keys[ALLEGRO_KEY_MAX];
+    ClearKeyboardKeys(keyboard_keys);
 
+    Combate estado_do_jogo;
     Renderer renderer;
-    Combate estado;
-    FillRenderer(&renderer);
 
-    inicio_combate(&estado);
-    renderer.estado = &estado;
+    FillRenderer(&renderer);
+    renderer.estado = &estado_do_jogo;
 
     al_register_event_source(queue, al_get_display_event_source(renderer.display));
-    al_register_event_source(queue, al_get_keyboard_event_source());
     al_register_event_source(queue, al_get_timer_event_source(timer));
+    al_register_event_source(queue, al_get_keyboard_event_source());
+
+    inicio_combate(&estado_do_jogo);
 
     al_start_timer(timer);
 
-    while (1) {
+    bool player_turn_started = false;
+    while (estado_do_jogo.jogador.dados_base.vida_atual > 0 &&
+           estado_do_jogo.num_combates_vencidos < 10) {
+
         al_wait_for_event(queue, &event);
+        bool done = false;
+        bool redraw = false;
 
-        if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
-            break;
+        switch (event.type) {
+            case ALLEGRO_EVENT_TIMER:
+                redraw = true;
 
-        if (event.type == ALLEGRO_EVENT_KEY_DOWN)
-            keys[event.keyboard.keycode] = GAME_KEY_DOWN | GAME_KEY_SEEN;
+                if (estado_do_jogo.vez == vez_jogador) {
+                    if (!player_turn_started) {
+                        inicio_turno(&estado_do_jogo.jogador);
+                        player_turn_started = true;
+                    }
+                    Processa_Input_e_Acao(&estado_do_jogo, keyboard_keys);
+                    if (estado_do_jogo.vez != vez_jogador) {
+                        player_turn_started = false;
+                    }
+                } else {
+                    turno_inimigos(&estado_do_jogo.inimigos, &estado_do_jogo.jogador);
+                    mudar_turno(&estado_do_jogo);
+                    player_turn_started = false;
+                }
 
-        if (event.type == ALLEGRO_EVENT_KEY_UP)
-            keys[event.keyboard.keycode] &= ~GAME_KEY_DOWN;
+                // atalho sair
+                if (keyboard_keys[ALLEGRO_KEY_Q] & GAME_KEY_DOWN) {
+                    done = true;
+                }
 
-        if (event.type == ALLEGRO_EVENT_TIMER) {
-            Processa_Input_e_Acao(&estado, keys);
-            ClearKeyboardKeys(keys);
+                ClearKeyboardKeys(keyboard_keys);
+                break;
+
+            case ALLEGRO_EVENT_KEY_DOWN:
+                keyboard_keys[event.keyboard.keycode] = GAME_KEY_SEEN | GAME_KEY_DOWN;
+                break;
+            case ALLEGRO_EVENT_KEY_UP:
+                keyboard_keys[event.keyboard.keycode] &= ~GAME_KEY_DOWN;
+                break;
+            case ALLEGRO_EVENT_DISPLAY_CLOSE:
+                done = true;
+                break;
+            default:
+                break;
+        }
+
+        if (done) break;
+
+        if (redraw) {
+            fim_combate(&estado_do_jogo);
             Render(&renderer);
         }
     }
 
+    al_destroy_timer(timer);
+    al_destroy_event_queue(queue);
     ClearRenderer(&renderer);
     return 0;
 }
